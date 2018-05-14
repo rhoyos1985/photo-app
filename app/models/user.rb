39a69ccdd7f58d6fd13342991aa0f0f2bc0d7d09
Richard Hoyos <richard@dentalzon.com>
2018-05-14 11:46:40 -0500

    Add stripe payment

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_one :payment
+  accepts_nested_attributes_for :payment # Permit create and update payment from user. Just used for has-one asociation
 end
